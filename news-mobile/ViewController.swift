089d788a6ab70c3058c604d53db547060fabd250
mfabianchm <mfchongm@hotmail.com>
2024-09-02 12:16:06 -0600

    Initial Commit

---
+++ news-mobile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  news-mobile
+//
+//  Created by Marcos Fabian Chong Megchun on 02/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
